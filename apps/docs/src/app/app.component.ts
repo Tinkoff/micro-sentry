301cf2779a11f5b7afb79be1ae565b5429dd3711
IKatsuba <katsuba.igor@gmail.com>
2021-09-24 22:22:52 +0300

    chore: add Angular Universal sandbox

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-  title = 'docs';
-}
+export class AppComponent {}
--- apps/docs/src/app/app.module.ts
+++ apps/docs/src/app/app.module.ts
@@ -1,12 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, Inject, NgModule } from '@angular/core';
+import { MicroSentryModule } from '@micro-sentry/angular';
 
 import { AppComponent } from './app.component';
+import { IS_BROWSER_PLATFORM, IS_SERVER_PLATFORM } from '@ngx-ssr/platform';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    MicroSentryModule.forRoot({
+      dsn:
+        'https://7cc7ac5332864c97abbccf20d90af60c@o1012545.ingest.sentry.io/5978131',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    errorHandler: ErrorHandler,
+    @Inject(IS_SERVER_PLATFORM) isServer: boolean,
+    @Inject(IS_BROWSER_PLATFORM) isBrowser: boolean
+  ) {
+    if (isServer) {
+      errorHandler.handleError(new Error('server'));
+    }
+
+    if (isBrowser) {
+      errorHandler.handleError(new Error('browser'));
+    }
+  }
+}
